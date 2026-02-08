69976b30f50445e611c8d5a685b0abd66c7dbbae
igor y <yakobchuk.igor@yahoo.com>
2026-02-09 00:23:18 +0200

    add exercise 01_02

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -3,12 +3,14 @@
 // Console Interaction, by Eduardo Corpeño 
 
 #include <iostream>
-#include <string>
+#include <string> // чтобы получить строку от пользователя вкл заголовочный файл string
 
 int main(){
-    std::string str;
-    std::cin >> str;
-    std::cout << str;
+    std::string str; //объявляем строковую переменную str и используем её для хранения пользовательского ввода. std::string — это тип, как int или double, только он хранит текст, а не число.
+    
+    std::cout << "Enter your name: " << std::flush;
+    std::cin >> str; //для ввода. После того как юзер введет данные, они будут в str
+    std::cout <<"Nice to meet you, "<< str << "!" << std::endl; //для вывода str
 
     std::cout << std::endl << std::endl;
     return (0);
