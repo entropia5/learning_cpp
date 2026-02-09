6b3d6236b44d84be7879efdcb0f9c687145ed941
igor y <yakobchuk.igor@yahoo.com>
2026-02-09 23:57:52 +0200

    type casting exercise

--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -6,7 +6,18 @@
 #include <cstdint>
 
 int main(){
-    
+    float flt;
+    int32_t sgn;
+    int32_t unsgn;
+
+   flt = - 2.55; 
+   sgn = flt;
+   sgn = unsgn;
+
+    std::cout << "float: " << flt << std::endl;
+    std::cout << "int32: " <<sgn << std::endl;
+    std::cout << "uint32: " <<unsgn<< std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
