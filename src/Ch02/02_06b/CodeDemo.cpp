6436bd4e25c254f9102c8e4c7c50f5bb7e26aff8
igor y <yakobchuk.igor@yahoo.com>
2026-02-09 06:18:02 +0200

    add exercise, define, #ifdef-#endif

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -3,9 +3,25 @@
 // Preprocessor directives, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string> //строковый заголовочный файл
+#include <cstdint> 
+
+#define CAPACITY 5000
+#define DEBUG
 
 int main(){
-    
+    int32_t large = CAPACITY;
+    uint8_t small = 37;
+#ifdef DEBUG //добавим условные операторы #ifdef, они работают в сочетании с определенными директивами
+
+    std::cout << "[About to perform the addition]" << std::endl;
+#endif
+    large += small; //large = large + small; 
+    std::cout << "The Large integer is " << large << std::endl;
+
+   
+
+
     std::cout << std::endl << std::endl;
     return (0);
 }
