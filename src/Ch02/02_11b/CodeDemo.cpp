02a041b0136379e276b520a12cc41515abe7dcc7
igor y <yakobchuk.igor@yahoo.com>
2026-02-10 00:27:08 +0200

    type casting exercise 2

--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -5,8 +5,26 @@
 #include <iostream>
 #include <cstdint>
 
+
+
 int main(){
+
+    int fahrenheit = 100;
+    int celsius;
+
+    celsius = ((float)5/ 9) * (fahrenheit - 32);
+
+    std::cout << std::endl;
+    std::cout <<"Fahrenheit: " << fahrenheit<< std::endl;
+    std::cout <<"Celsius: " << celsius<< std::endl;
     
+    float weight = 10.99;
+
+    std::cout << std::endl;
+    std::cout <<"Float: " << weight<< std::endl;
+    std::cout <<"Integer part: " << (int) weight<< std::endl;
+    std::cout <<"Fractional part: " << weight - (int) weight << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
