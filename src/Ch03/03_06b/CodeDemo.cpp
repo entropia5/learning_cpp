38595ba32d9a9afdff95aededa25cc0c2a5d408c
igor y <yakobchuk.igor@yahoo.com>
2026-02-10 09:11:02 +0200

    pointer exercise exercise

--- src/Ch03/03_06b/CodeDemo.cpp
+++ src/Ch03/03_06b/CodeDemo.cpp
@@ -6,6 +6,16 @@
 #include <string>
 
 int main(){
+int a = 37;
+int *ptr;
+
+ptr = &a;
+
+    std::cout << "a: " << a << std::endl;
+    std::cout << "ptr: " << ptr << std::endl;
+    std::cout << "&a: " << &a << std::endl;
+    std::cout << " *ptr : " << *ptr << std::endl;
+    std::cout << " &ptr : " << &ptr << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
