62aa6b5fddfb70cb874704c857a5d68f2cc74092
igor y <yakobchuk.igor@yahoo.com>
2026-02-10 07:29:10 +0200

    update theory 6f

--- src/Ch03/03_01b/CodeDemo.cpp
+++ src/Ch03/03_01b/CodeDemo.cpp
@@ -5,8 +5,33 @@
 #include <iostream>
 #include <string>
 
+enum class cow_purpose {dairy, meat, hide, pat};
+
+//смоделировали корову с помощью структуры
+/* struct cow {                                
+    std::string name;
+    int age;
+    cow_purpose purpose;
+};
+*/
+//классы структуры могут быть очень похожи
+class cow {                  
+public:              
+    std::string name;
+    int age;
+    cow_purpose purpose;
+};
+
+
 int main(){
+    cow my_cow;
+    my_cow.age = 5;
+    my_cow.name = "Betsy";
+    my_cow.purpose = cow_purpose::dairy;
+    std::cout<< my_cow.name << " is a type-" << (int) my_cow.purpose << ' cow.' << std::endl;
     
+
+
     std::cout << std::endl << std::endl;
     return (0);
 }
